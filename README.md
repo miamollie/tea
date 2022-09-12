ca34ed6004ba7c8ddc4a86cf884eaa4bfba52da2
miamollie <miamollie@gmail.com>
2022-09-12 20:55:39 +1000

    Initial setup

--- README.md
+++ README.md
@@ -1,2 +1,35 @@
 # tea
-Example of OpenAPI 
+Example of Golang api using OpenAPI schemas
+
+Types generated using `https://github.com/deepmap/oapi-codegen/`
+
+
+## Development
+
+Updating the schema
+
+1. Add to the `schema.yml` file
+1. run `go generate ./...` 
+
+
+### Running the app
+
+Bring up the app
+
+`go run main.go`
+
+test the endpoints with curl or in vscode using the `requests.http` files
+
+```
+curl --request GET --url http://localhost:8080/teas
+curl --request POST --url http://localhost:8080/teas
+curl --request GET --url http://localhost:8080/teas/{id}
+
+
+```
+
+
+### TODO
+- requests.http
+- docker
+- deploy
---
+++ api/teas.go
@@ -0,0 +1,31 @@
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.tea.yml ./tea.yml
+//go:generate go run github.com/deepmap/oapi-codegen/cmd/oapi-codegen --config=config.schema.yml ./schema.yml
+
+package api
+
+import (
+	"encoding/json"
+	"net/http"
+)
+
+type TeaService struct {
+	Teas map[int64]Tea
+}
+
+type Tea struct {
+	Id          int32
+	Name        string
+	Description string
+}
+ 
+
+func NewTeaService(teas map[int64]Tea) *TeaService {
+	return &TeaService{
+		Teas: teas,
+	}
+}
+
+func (ts *TeaService) GetTeas(w http.ResponseWriter, r *http.Request) {
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(ts.Teas)
+}
