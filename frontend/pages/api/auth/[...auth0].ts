01fb79a60f57379f16d953706087858186623792
miamollie <miamollie@gmail.com>
2023-09-11 20:36:01 +1000

    Ensure base url set correctly

--- frontend/pages/api/auth/[...auth0].ts
+++ frontend/pages/api/auth/[...auth0].ts
@@ -1,4 +1,21 @@
-import { handleAuth } from "@auth0/nextjs-auth0";
+import {
+  handleAuth,
+  handleCallback,
+  handleLogin,
+  handleLogout,
+} from "@auth0/nextjs-auth0";
 
-export default handleAuth();
+const AUTH0_BASE_URL =
+  process.env.AUTH0_BASE_URL || process.env.NEXT_PUBLIC_AUTH0_BASE_URL;
 
+const redirectUri = `${AUTH0_BASE_URL}/api/page-router-auth/callback`;
+
+export default handleAuth({
+  login: handleLogin({
+    authorizationParams: { redirect_uri: redirectUri },
+  }),
+  callback: handleCallback({ redirectUri }),
+  logout: handleLogout({
+    returnTo: `${AUTH0_BASE_URL}/page-router`,
+  }),
+});
